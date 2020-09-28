df07bbc69b47d39c18690b6c0306a09dc7a051f6
Chenuet Loic <loicchenuet@hotmail.fr>
2020-09-28 22:21:15 +0200

    first commit

---
+++ codeIndiana.php
@@ -0,0 +1,7 @@
+
+<?php
+
+function writeSecretSentence(string $PARAMETRE_1, string $PARAMETRE_2) {
+    return $PARAMETRE_1 . ' s\'incline face à ' . $PARAMETRE_2 . ' en remplaçant ' . $PARAMETRE_1 . ' et '. $PARAMETRE_2;
+}
+echo writeSecretSentence('lion', 'singe');
