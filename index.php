97a0a9f4a42df98dc3d62504dcc093b3ac029887
ChenuetLoic <68080281+ChenuetLoic@users.noreply.github.com>
2020-09-29 13:09:36 +0200

    Update index.php
    Rajout string

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 
 <?php
 
-function writeSecretSentence(string $PARAMETRE_1, string $PARAMETRE_2) {
+function writeSecretSentence(string $PARAMETRE_1, string $PARAMETRE_2):string {
     return $PARAMETRE_1 . ' s\'incline face à ' . $PARAMETRE_2 . ' en remplaçant ' . $PARAMETRE_1 . ' et '. $PARAMETRE_2;
 }
 echo writeSecretSentence('lion', 'singe');
